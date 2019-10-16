43e991394dfb992390394bf59c1b39c1985dfae8
rafaeldias97 <rafael.cdc97@gmail.com>
2019-10-16 10:28:59 -0300

    Swagger

--- APIModeloDDD/APIModeloDDD.Presentation/Startup.cs
+++ APIModeloDDD/APIModeloDDD.Presentation/Startup.cs
@@ -1,16 +1,13 @@
- using System;
-using System.IO;
-using System.Reflection;
 using APIModeloDDD.Infra.CrossCutting;
-using APIModeloDDD.Presentation.Swagger;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Swashbuckle.AspNetCore.Examples;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
 
 namespace APIModeloDDD.Presentation
 {
@@ -28,41 +25,27 @@ namespace APIModeloDDD.Presentation
         {
             services.AddControllers();
 
-            //Configuração do Swagger
-            //services.ConfigureSwaggerGen(x =>
-            //{
-            //    x.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
-            //    x.OperationFilter<ProducesOperatioFilter>();
-            //});
-
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new Info
-            //    {
-            //        Version = "v1",
-            //        Title = "API Modelo DDD",
-            //        Description = "Exemplo de API usando padrão DDD",
-            //        TermsOfService = "None",
-            //        Contact = new Contact
-            //        {
-            //            Name = "Rafael Dias",
-            //            Email = string.Empty,
-            //            Url = "https://github.com/rafaeldias97"
-            //        }
-            //    });
-
-            //    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            //    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            //    c.IncludeXmlComments(xmlPath);
-
-            //    c.OperationFilter<ExamplesOperationFilter>();
-            //});
-
             //AutoMapper
             services.AddAutoMapper(typeof(Startup));
 
             // IoC
             NativeInjectorBootStrapper.RegisterServices(services, Configuration);
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "API Modelo DDD",
+                    Version = "v1",
+                    Description = "A Base API using DDD(Domain Driven Design)",
+                    Contact = new OpenApiContact {
+                        Name = "Rafael Dias",
+                        Email = "rafael.cdc97@gmail.com",
+                        Url = new Uri("https://github.com/rafaeldias97")
+                    }
+                });
+                var filePath = Path.Combine(AppContext.BaseDirectory, "APIModeloDDD.Presentation.xml");
+                x.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,13 +56,6 @@ namespace APIModeloDDD.Presentation
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseSwagger();
-
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint($"../swagger/v1/swagger.json", "apimodelo");
-            //});
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -89,6 +65,13 @@ namespace APIModeloDDD.Presentation
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Modelo DDD");
             });
         }
     }

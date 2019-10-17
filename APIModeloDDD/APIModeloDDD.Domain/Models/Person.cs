348234e3dc4df4c5e18c41a16211e973bd16ce7a
rafaeldias97 <rafael.cdc97@gmail.com>
2019-10-17 09:14:09 -0300

    remove age

--- APIModeloDDD/APIModeloDDD.Domain/Models/Person.cs
+++ APIModeloDDD/APIModeloDDD.Domain/Models/Person.cs
@@ -10,18 +10,5 @@ namespace APIModeloDDD.Domain.Models
         public string email { get; set; }
         public string senha { get; set; }
         public DateTime birthdate { get; set; }
-
-        public int idade()
-        {
-            // Save today's date.
-            var today = DateTime.Today;
-            // Calculate the age.
-            var age = today.Year - this.birthdate.Year;
-            // Go back to the year the person was born in case of a leap year
-            if (this.birthdate.Date > today.AddYears(-age)) age--;
-
-            return age;
-        }
-
     }
 }
